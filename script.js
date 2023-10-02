0a0db4cdd27a7dc56df6147ada9088f3ca012412
Rishabh Vishwakarma <ectopdash@gmail.com>
2023-10-02 11:48:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,35 +7,41 @@ const monthOut = document.getElementById('monthOut');
 const yearOut = document.getElementById('yearOut');
 const calculateBtn = document.getElementById('calculateBtn');
 const errorStyle = '0.5px solid var(--Light-red)';
+
 // Calculate Button
 calculateBtn.addEventListener('click', () => {
-  const D = dayIn.value;
-  const M = monthIn.value;
-  const Y = yearIn.value;
-  const birthday = `${Y}-${M}-${D}`;
-  if (validateDay() && validateMonth() && validateYear()) {
+  const day = dayIn.value;
+  const month = monthIn.value;
+  const year = yearIn.value;
+  const birthday = `${year}-${month}-${day}`;
+
+  if (validateDay(day, month, year) && validateMonth(month) && validateYear(year, month, day)) {
     console.log('Done');
   } else {
     return;
   }
+
   const birthDate = new Date(birthday);
   const currentDate = new Date();
   console.log(birthDate.getTime());
   const timeDiff = currentDate.getTime() - birthDate.getTime();
+
   // Age Calculation
   let years = new Date().getFullYear() - new Date(birthday).getFullYear();
   let months = new Date().getMonth() - new Date(birthday).getMonth();
-  let days = new Date().getDate() - Number(D);
+  let days = new Date().getDate() - Number(day);
   const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
-  // Display Values
+
+  // Handle negative months and days
   if (months < 0) {
     years = years - 1;
     months = months + 12;
   }
   if (days < 0) {
-    days += getNoOfDays(Y, M - 1);
+    days += getNoOfDays(year, month - 1);
   }
+
   // Display Values
   dayOut.innerText = days;
   monthOut.innerText = months;
@@ -43,86 +49,71 @@ calculateBtn.addEventListener('click', () => {
   document.getElementById('hourOut').innerText = hours;
   document.getElementById('minuteOut').innerText = minutes;
 });
-// Get Number of Days in a particular months
-function getNoOfDays(y, m) {
-  return new Date(y, m, 0).getDate();
+
+// Get Number of Days in a particular month
+function getNoOfDays(year, month) {
+  return new Date(year, month, 0).getDate();
 }
+
 /*================ on Blur Validation =========================*/
+
 // On Blur day validation
 dayIn.addEventListener('blur', () => {
-  validateDay();
+  validateDay(dayIn.value, monthIn.value, yearIn.value);
 });
-// Validate Day function
-const validateDay = () => {
-  const D = dayIn.value;
-  const M = monthIn.value;
-  const Y = yearIn.value;
-  if (D == '') {
+
+// On Blur month validation
+monthIn.addEventListener('blur', () => {
+  validateMonth(monthIn.value);
+});
+
+// On Blur Year validate
+yearIn.addEventListener('blur', () => {
+  validateYear(yearIn.value, monthIn.value, dayIn.value);
+});
+
+// Validate Day
+function validateDay(day, month, year) {
+  if (day == '') {
     showMessage(dayIn, 'This field is required', errorStyle);
     return false;
-  } else if (!validDay(Y, M, D)) {
+  } else if (!validDay(year, month, day)) {
     showMessage(dayIn, 'Must be a valid day', errorStyle);
     return false;
   } else {
     showMessage(dayIn, '', '');
     return true;
   }
-};
-// On Blur month validation
-monthIn.addEventListener('blur', () => {
-  validateMonth();
-});
-const validateMonth = () => {
-  const M = monthIn.value;
-  if (M == '') {
+}
+
+// Validate Month
+function validateMonth(month) {
+  if (month == '') {
     showMessage(monthIn, 'This field is required', errorStyle);
     return false;
-  } else if (!validMonth(M)) {
+  } else if (!validMonth(month)) {
     showMessage(monthIn, 'Must be a valid month', errorStyle);
     return false;
   } else {
     showMessage(monthIn, '', '');
     return true;
   }
-};
-// on Blur Year validate
-yearIn.addEventListener('blur', () => {
-  validateYear();
-});
-const validateYear = () => {
-  const Y = yearIn.value;
-  const M = monthIn.value;
-  const D = dayIn.value;
-  if (Y == '') {
+}
+
+// Validate Year
+function validateYear(year, month, day) {
+  if (year == '') {
     showMessage(yearIn, 'This field is required', errorStyle);
     return false;
-  } else if (!validYear(Y, M, D)) {
-    showMessage(yearIn, 'Must be in past', errorStyle);
+  } else if (!validYear(year, month, day)) {
+    showMessage(yearIn, 'Must be in the past', errorStyle);
     return false;
   } else {
     showMessage(yearIn, '', '');
     return true;
   }
-};
-// Validate Day
-function validDay(y, m, d) {
-  if (d > getNoOfDays(y, m) || d < 1) return false;
-  return true;
 }
-// validate Month
-function validMonth(m) {
-  if (m > 12 || m < 1) return false;
-  return true;
-}
-// Validate Year
-function validYear(y, m, d) {
-  const secondDate = new Date();
-  const firstDate = new Date(`${y}-${m}-${d}`);
-  if (firstDate.setHours(0, 0, 0, 0) <= secondDate.setHours(0, 0, 0, 0)) {
-    return true;
-  }
-  return false;
-}
+
 // Display Message
 function showMessage(elem, msg, border) {
   elem.style.border = border;
